c9ba474a752a05b9428502e0364d896e13c44490
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-12 22:25:23 +0200

    Add treatment start and end date

--- synthetic_control/model.py
+++ synthetic_control/model.py
@@ -8,13 +8,22 @@ from sklearn.linear_model import Ridge
 class SyntheticControl:
     """A model to use the synthetic control method."""
 
-    def __init__(self, force_positive=True, fit_intercept=False):
-        self.force_positive = force_positive
-        self.fit_intercept = fit_intercept
-        self.model = self.setup_model()
+    def __init__(
+        self,
+        treatment_start,
+        treatment_end=None,
+        force_positive=True,
+        fit_intercept=False,
+    ):
+        self.treatment_start = treatment_start
+        self.treatment_end = treatment_end
+        self.model = self.setup_model(force_positive, fit_intercept)
 
-    def setup_model(self):
-        return Ridge(fit_intercept=self.fit_intercept, positive=self.force_positive)
+    def setup_model(self, force_positive, fit_intercept):
+        return Ridge(
+            positive=force_positive,
+            fit_intercept=fit_intercept,
+        )
 
     def fit(self):
         pass
