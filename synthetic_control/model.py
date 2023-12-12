cacf50ab6298d671f698fb2bd3ded04b1c881f35
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-12 22:03:37 +0200

    Add model

---
+++ synthetic_control/model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+""" A class to use the synthetic control method. """
+
+
+class SyntheticControl:
+    """A model to create."""
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def fit_predict(self):
+        pass
