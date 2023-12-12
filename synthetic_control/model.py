beb647df249928ed6c0591f5e04e494a7d42511c
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-12 22:04:00 +0200

    Add model

--- synthetic_control/model.py
+++ synthetic_control/model.py
@@ -2,12 +2,14 @@
 
 """ A class to use the synthetic control method. """
 
+from sklearn.linear_model import Ridge
+
 
 class SyntheticControl:
-    """A model to create."""
+    """A model to use the synthetic control method."""
 
     def __init__(self):
-        pass
+        self.model = Ridge(fit_intercept=False, positive=True)
 
     def fit(self):
         pass
