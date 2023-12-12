3fe40bd97f415855cca3cb2b0a6d5b701f2010fe
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-12 22:54:49 +0200

    Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,6 +2,8 @@
 
 """ Tests for synthetic_control.model. """
 
+from datetime import datetime
+
 from sklearn.linear_model import Ridge
 
 from synthetic_control.model import SyntheticControl
@@ -13,8 +15,18 @@ class TestModel:
         assert isinstance(sc, SyntheticControl)
 
     def test_setup_model(self):
-        sc = SyntheticControl(fit_intercept=True, force_positive=False)
-        sc.setup_model()
+        sc = SyntheticControl(
+            treatment_start=datetime(2009, 1, 1), treatment_end=datetime(2010, 1, 1)
+        )
+        sc._setup_model()
         assert isinstance(sc.model, Ridge)
-        assert sc.model.fit_intercept is True
+        assert sc.treatment_start == datetime(2009, 1, 1)
+        assert sc.model.positive == datetime(2010, 1, 1)
+
+    def test_create_treatment_phases(self):
+        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc._setup_model(alpha=0.05, positive=False, fit_intercept=True)
+        assert isinstance(sc.model, Ridge)
+        assert sc.model.alpha == 0.05
         assert sc.model.positive is False
+        assert sc.model.fit_intercept is True
