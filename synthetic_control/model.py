2646a4c4cea6f0b968ae2ea2d08e828abd5cef59
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-12 22:16:15 +0200

    Create fitting model

--- synthetic_control/model.py
+++ synthetic_control/model.py
@@ -8,8 +8,15 @@ from sklearn.linear_model import Ridge
 class SyntheticControl:
     """A model to use the synthetic control method."""
 
-    def __init__(self):
-        self.model = Ridge(fit_intercept=False, positive=True)
+    def __init__(self, force_positive=True, fit_intercept=False):
+        self.force_positive = force_positive
+        self.fit_intercept = fit_intercept
+        self.model = self.setup_model()
+
+    def setup_model(self):
+        self.model = Ridge(
+            fit_intercept=self.fit_intercept, positive=self.force_positive
+        )
 
     def fit(self):
         pass
