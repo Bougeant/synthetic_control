ed406209be2cb30aac3e7d9ae9b2f9cdafda07e1
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-13 22:21:53 +0200

    Fix tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -33,24 +33,24 @@ class TestSyntheticControl:
 
     def test_create(self):
         """Test the creation of a SyntheticControl object."""
-        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc = SyntheticControl(treatment_start=datetime(2008, 1, 1))
         assert isinstance(sc, SyntheticControl)
 
     def test_setup_model(self):
         """Test the setup_model method."""
         sc = SyntheticControl(
-            treatment_start=datetime(2009, 1, 1), treatment_end=datetime(2010, 1, 1)
+            treatment_start=datetime(2008, 1, 1), treatment_end=datetime(2010, 1, 1)
         )
         sc._setup_model()
         assert isinstance(sc.model, Ridge)
-        assert sc.treatment_start == datetime(2009, 1, 1)
+        assert sc.treatment_start == datetime(2008, 1, 1)
         assert sc.treatment_end == datetime(2010, 1, 1)
 
     def test_get_treatment_phase(self):
         """Test the _create_treatment_phases method."""
         X, y = self.get_test_data()
         sc = SyntheticControl(
-            treatment_start=datetime(2009, 1, 1), treatment_end=datetime(2011, 1, 1)
+            treatment_start=datetime(2008, 1, 1), treatment_end=datetime(2010, 1, 1)
         )
         treatment = sc._get_treatment_phase(X)
         assert np.allclose(treatment, [False] * 3 + [True] * 2 + [False] * 2)
@@ -58,21 +58,21 @@ class TestSyntheticControl:
     def test_get_treatment_phase_no_end(self):
         """Test the _create_treatment_phases method."""
         X, y = self.get_test_data()
-        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc = SyntheticControl(treatment_start=datetime(2008, 1, 1))
         treatment = sc._get_treatment_phase(X)
         assert np.allclose(treatment, [False] * 3 + [True] * 4)
 
     def test_fit(self):
         """Test the fit method."""
         X, y = self.get_test_data()
-        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc = SyntheticControl(treatment_start=datetime(2008, 1, 1))
         sc.fit(X, y)
         check_is_fitted(sc.model)
 
     def test_predict(self):
         """Test the predict method."""
         X, y = self.get_test_data()
-        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc = SyntheticControl(treatment_start=datetime(2008, 1, 1))
         sc.fit(X, y)
         y_synth = sc.predict(X)
         np.allclose(y_synth, [100.0, 97.1, 95.91, 106.6, 96.1, 93.9, 96.6], atol=0.1)
@@ -80,7 +80,7 @@ class TestSyntheticControl:
     def test_fit_predict(self):
         """Test the fit_predict method."""
         X, y = self.get_test_data()
-        sc = SyntheticControl(treatment_start=datetime(2009, 1, 1))
+        sc = SyntheticControl(treatment_start=datetime(2008, 1, 1))
         y_synth = sc.fit_predict(X, y)
         np.allclose(y_synth, [100.0, 97.1, 95.91, 106.6, 96.1, 93.9, 96.6], atol=0.1)
 
@@ -88,16 +88,16 @@ class TestSyntheticControl:
         """Test the get_confidence_interval method."""
         X, y = self.get_test_data()
         sc = SyntheticControl(
-            treatment_start=datetime(2009, 1, 1),
+            treatment_start=datetime(2008, 1, 1),
             ci_fraction=0.6,
             ci_percentiles=[5, 95],
         )
         y_ci = sc.get_confidence_interval(X, y)
         expected_df = pd.DataFrame(
             {
-                5: [86.03, 76.70, 55.21, 45.35, 89.81, 84.82, 77.10],
-                95: [105.12, 108.86, 93.48, 123.64, 106.46, 123.66, 95.28],
+                5: [96.8, 81.2, 71.4, 51.4, 86.5, 70.7, 82.3],
+                95: [118.1, 101.6, 100.1, 169.9, 121.1, 140.7, 98.2],
             },
             index=X.index,
         )
-        assert_frame_equal(y_ci, expected_df, atol=0.01)
+        assert_frame_equal(y_ci, expected_df, atol=0.1)
