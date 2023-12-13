2b4ac0a6c0db0eefdb2352c0dc7bde3ccbaa16b2
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-13 08:50:21 +0200

    Add treatment start end and name

--- synthetic_control/analysis.py
+++ synthetic_control/analysis.py
@@ -5,14 +5,21 @@
 import plotly.graph_objects as go
 
 
-def display_synthetic_control(y, y_pred, y_pred_ci):
+def display_synthetic_control(y, y_pred, y_pred_ci, treatment_start, **kwargs):
     """ """
-    data = _get_plot_data(y, y_pred, y_pred_ci)
+    data = _get_plot_data(y, y_pred, y_pred_ci, treatment_start, **kwargs)
     layout = _get_plot_layout()
     return go.Figure(data=data, layout=layout)
 
 
-def _get_plot_data(y, y_pred, y_pred_ci):
+def _get_plot_data(
+    y,
+    y_pred,
+    y_pred_ci,
+    treatment_start,
+    treatment_end=None,
+    treatment_name="Treatment",
+):
     """ """
     data = [
         go.Scatter(x=y.index, y=y),
