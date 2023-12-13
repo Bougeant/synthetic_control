593b403dedf4f47aa149b1ea1dbbb62c0113cc0b
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-13 08:55:41 +0200

    Add layout

--- synthetic_control/analysis.py
+++ synthetic_control/analysis.py
@@ -5,10 +5,12 @@
 import plotly.graph_objects as go
 
 
-def display_synthetic_control(y, y_pred, y_pred_ci, treatment_start, **kwargs):
+def display_synthetic_control(
+    y, y_pred, y_pred_ci, treatment_start, y_axis="Value", **kwargs
+):
     """ """
     data = _get_plot_data(y, y_pred, y_pred_ci, treatment_start, **kwargs)
-    layout = _get_plot_layout()
+    layout = _get_plot_layout(y_axis)
     return go.Figure(data=data, layout=layout)
 
 
@@ -22,7 +24,16 @@ def _get_plot_data(
 ):
     """ """
     data = [
-        go.Scatter(x=y.index, y=y),
-        go.Scatter(x=y.index, y=y_pred),
+        go.Scatter(x=y.index, y=y, name=treatment_name),
+        go.Scatter(x=y.index, y=y_pred, name="Synthetic Control"),
     ]
     return data
+
+
+def _get_plot_layout(y_axis):
+    """ """
+    layout = go.Layout(
+        xaxis={"title": "Date"},
+        yaxis={"title": y_axis},
+    )
+    return layout
