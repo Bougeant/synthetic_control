84450a2d6c6e7fb1189872fb42a7b0580fefd69a
Olivier Bougeant <olivier.bougeant@prodigyfinance.com>
2023-12-14 23:33:26 +0200

    Bump version: 0.0.1 → 0.0.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ jupyter_requirements = ["jupyter", "jupyterlab<4", "jupytext"]
 
 setup(
     name="synthetic-control",
-    version="0.0.1",
+    version="version="0.0.1"",
     url="http://github.com/Bougeant/synthetic_control",
     license_files=("LICENSE.txt",),
     description=("A user friendly package to use the Synthetic Control Method"),
